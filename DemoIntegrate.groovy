3511538dae6dc14c186eae56995f44b7afa17519
David Clark <david@Augustine.(none)>
2013-02-05 00:23:43 -0600

    Added comments and better output

--- DemoIntegrate.groovy
+++ DemoIntegrate.groovy
@@ -3,24 +3,38 @@ import groovyx.gpars.*;
 Integrate.installToMath();
 
 //demonstrate integration strategies
-println(Integrate.rectangular(0.0d, Math.PI, 2000, Math.&sin as SingleFunc));
-println(Integrate.trapezoidal(0.0d, Math.PI, 2000, Math.&sin as SingleFunc));
+println("sin(x), [x: 0 -> PI]: " +
+	Integrate.rectangular(0.0d, Math.PI, 2000, Math.&sin as SingleFunc));
+println("sin(x), [x: 0 -> PI]: " +
+	Integrate.trapezoidal(0.0d, Math.PI, 2000, Math.&sin as SingleFunc));
 
 //demonstrate parallel integration
 final int PROCESSORS = Runtime.getRuntime().availableProcessors();
 def pool = GParsPool.createPool(PROCESSORS + 1);
-println(Integrate.integrateParallel(lower: 0.0d, upper: Math.PI, pool: pool,
+
+println("sin(x), [x: 0 -> PI]: " +
+	Integrate.integrateParallel(lower: 0.0d, upper: Math.PI, pool: pool,
 				    steps: 2000, func: Math.&sin as SingleFunc));
-println(Integrate.integrateParallel(lower: -(Math.PI/2), upper: Math.PI/2, pool: pool,
+println("cos(x), [x: -PI/2 -> PI/2]: " +
+	Integrate.integrateParallel(lower: -(Math.PI/2), upper: Math.PI/2, pool: pool,
 				    steps: 2000, func: Math.&cos as SingleFunc));
-println(Integrate.integrateParallel(lower: -(Math.PI/4), upper: Math.PI/4, pool: pool,
+
+//demonstrate different function on a different interval
+println("tan(x), [x: -PI/4 -> PI/4]: " +
+	Integrate.integrateParallel(lower: -(Math.PI/4), upper: Math.PI/4, pool: pool,
 				    steps: 2000, func: Math.&tan as SingleFunc));
-println(Integrate.integrateParallel(lower: 3, upper: 7, pool: pool,
+
+//Note, even though SingleFunc is defined as @CompileStatic,
+//we can still be lazy and use groovy closures
+println("x*x, [x: 3 -> 7]: " +
+	Integrate.integrateParallel(lower: 3, upper: 7, pool: pool,
 				    steps: 2000, func: { double x -> return x * x } as SingleFunc));
 
 //use enhanced java.util.Math class
-println(Math.integrate(lower: 0.0d, upper: Math.PI, steps: 2000,
+println("sin(x), [x: 0 -> PI]: " +
+	Math.integrate(lower: 0.0d, upper: Math.PI, steps: 2000,
 		       func: Math.&sin as SingleFunc));
-println(Math.integrateParallel(lower: 3, upper: 7, pool: pool,
+println("x*x, [x: 3 -> 7]: " +
+	Math.integrateParallel(lower: 3, upper: 7, pool: pool,
 			       steps: 2000, func: { double x -> return x * x } as SingleFunc));
 
